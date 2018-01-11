20e550a2fb7e704f321ba723baa8060736dbafc3
Daniel Baamonde <dannyb21892@gmail.com>
2018-01-11 04:13:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,9 +65,10 @@ function placeOrder(cardNumber) {
     return("Sorry, we don't have a credit card on file for you.")
   }
   else {
-    return(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
+    var output = (`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
   }
   cart = [];
+  return(output)
 }
 
 addToCart("bananas");
