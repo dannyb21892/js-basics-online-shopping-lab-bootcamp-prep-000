4ca8267826f3a3b4a2304914b7103638c6064b30
Daniel Baamonde <dannyb21892@gmail.com>
2018-01-11 03:57:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (arguments.length < 1) {
+    return("Sorry, we don't have a credit card on file for you.")
+  }
+  else {
+    return(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
+  }
 }
-
-console.log(true*"yes" + false*"no")
