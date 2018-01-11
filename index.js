0584d6a29db4cb7c1bfda0f47890ba7801a5441c
Daniel Baamonde <dannyb21892@gmail.com>
2018-01-11 04:38:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,9 +70,9 @@ function placeOrder(cardNumber) {
     return;
   }
   else {
-    var total = total() = `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    var total = total()
+    cart = [];
+    console.log(`Your total cost is ${total}, which will be charged to the card ${cardNumber}.`);
+    return;
   }
-  cart = [];
-  console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
-  return;
 }
